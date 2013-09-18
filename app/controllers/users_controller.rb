060bd44d12a1b22ec4e1ddfb91db9238c5c8ec8b
Adam Keenan <adam_keenan@taylor.edu>
2013-09-18 16:58:17 +0000

    Added secure password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
     def new
-        @user = User.new
+      @user = User.new
     end
+
     def create
+      @user = User.new(params[:username])
+      @user.save
     end
 end
